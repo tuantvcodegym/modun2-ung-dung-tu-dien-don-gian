4a1eff0a0c69a96d9beda4270b3252cc13aff9ea
tuan tran <tuank11htt@gmail.com>
2019-01-22 09:20:32 +0700

    file

---
+++ ungdungtudiendongian.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>Ung dung tu dien don gian</title>
+</head>
+<body>
+<form method="post">
+    <input type="text" name="search" placeholder="search"/>
+    <input type="submit" value="Tim kiem"/>
+</form>
+<?php
+   $dictionary = ["hello"=>"xin chao","how"=>"the nao","book"=>"quyen vo","computer"=>"may tinh"];
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+        $searchword = $_POST["search"];
+        $flag = 0;
+        foreach ($dictionary as $index => $description){     //$dictionary la mang can duyet. $index vi tri cua phan tu.
+            if($index == $searchword){                       //$description: la value cua vi tri index.
+                echo "Tu: " .$index . ".</br> Nghia cua tu: " .$description;
+                echo "</br>";
+                $flag =1;
+            }
+        }
+        if($flag == 0){
+            echo "khong tim thay tu can tra";
+        }
+    }
+?>
+</body>
+</html>
